f570895926e1c91d92868db711a968fefac7e0a1
Grimmyxiii <bradchundage@gmail.com>
2023-01-11 14:30:52 -0700

    All Done

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,13 @@
 import React from "react"
 import { useState } from "react"
 import ReactMapGL from "react-map-gl"
+import { Marker, Popup } from "react-map-gl"
 import getCenter from "geolib/es/getCenter"
+import "mapbox-gl/dist/mapbox-gl.css"
 
 function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({})
+
   const coordinates = searchResults.map((result) => ({
     longitude: result.long,
     latitude: result.lat,
@@ -25,7 +29,40 @@ function Map({ searchResults }) {
       mapboxAccessToken={process.env.mapbox_key}
       {...viewport}
       onMove={(evt) => setViewport(evt.viewport)}
-    ></ReactMapGL>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              role="img"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce opacity-75"
+              aria-label="push-pin"
+            >
+              📌
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation(result)}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
   )
 }
 
